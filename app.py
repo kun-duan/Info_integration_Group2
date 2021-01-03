50dbf5b86dedd493eccbc35e1b20b721292ad044
jingshuohang <429342279@qq.com>
2021-01-03 10:47:15 +0800

    景朔杭提交成果啦

--- app.py
+++ app.py
@@ -1,11 +1,57 @@
 from flask import Flask
-
+from flask import request
+from flask import render_template
+from flask import jsonify
+#from jieba.analyse import  extract_tags
+import string
+import utils
+import gruop_sum_geo
 app = Flask(__name__)
 
+u=utils.utils_ertong("care","geoinformation","root","1234")
+fumu=utils.utils_fumu("care","geoinformation","root","1234")
+lingyanglv=utils.utils_lingyanglv("care","geoinformation","root","1234")
+renew_geo_sql=gruop_sum_geo.renew_sql()
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("main(1).html")
+
+@app.route('/geo_renew')
+def get_geo_renw():
+    renew_geo_sql.get_geo_sql()
+    return render_template("main(1).html")
+
+@app.route('/c2')
+def get_c2_data():
+    res=[]
+    data=u.get_c2_data()
+    for key,value in data.items():
+        res.append({"name":key,"value":value})
+    #for key,value in data_2.items():
+     #   res_2.append({"name":key,"value":value})
+      #  print(res)
+    return jsonify({"data":res})
+
+@app.route('/c2_fumu')
+def get_c2_data_fumu():
+    res=[]
+    data=fumu.get_c2_data_fumu()
+    for key,value in data.items():
+        res.append({"name":key,"value":value})
+    #for key,value in data_2.items():
+    #   res_2.append({"name":key,"value":value})
+    #  print(res)
+    return jsonify({"data":res})
+
+@app.route('/r1')
+def get_r1_data():
+    data=lingyanglv.get_r1_data()
+    keys=list(data.keys())
+    values=list(data.values())
+    print(jsonify({"keys":keys,"values":values}))
+    return jsonify({"keys":keys,"values":values})
+
 
 
 if __name__ == '__main__':
---
+++ static/js/ec_center.js
@@ -0,0 +1,62 @@
+var ec_center = echarts.init(document.getElementById('c2'), "dark");
+
+var mydata = [{'name': '上海', 'value': 318,'color':'#009fe8'}, {'name': '云南', 'value': 162,'color':'#009fe8'}]
+//各省地图颜色数据依赖value
+var ec_center_option = {
+    title: {
+        text: '',
+        subtext: '',
+        x: 'left'
+    },
+    tooltip: {
+        trigger: 'item'
+    },
+    //左侧小导航图标
+    visualMap: {
+        show: true,
+        x: 'left',
+        y: 'bottom',
+        textStyle: {
+            fontSize: 5,
+        },
+        splitList: [{ start: 1,end: 4999 },
+            {start: 5000, end: 9999 },
+            {start: 10000, end: 14999 },
+            { start: 15000, end: 19999 },
+            {  start: 20000, end: 24999 },
+            { start: 25000 }],
+        color: ['#DC143C', '#FF1493', '#ba55d3', '#45b97c', '#33a3dc','#426ab3']
+    },
+    //配置属性
+    series: [{
+        name: '孤儿数量',
+        type: 'map',
+        mapType: 'china',
+        roam: false, //拖动和缩放
+        itemStyle: {
+            normal: {
+                borderWidth: .5, //区域边框宽度
+                borderColor: '#009fe8', //区域边框颜色
+                areaColor: "#ffefd5", //区域颜色
+            },
+            emphasis: { //鼠标滑过地图高亮的相关设置
+                borderWidth: .5,
+                borderColor: '#4b0082',
+                areaColor: "#fff",
+            }
+        },
+        label: {
+            normal: {
+                show: true, //省份名称
+                fontSize: 6,
+            },
+            emphasis: {
+                show: true,
+                fontSize: 8,
+            }
+        },
+        data: []  //mydata //数据
+    }],
+
+};
+ec_center.setOption(ec_center_option)
